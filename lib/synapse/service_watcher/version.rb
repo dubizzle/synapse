67fd5c99604b8cd3557599743c7f14bf70fcbcf5
Qasim Khan <qasim@dubizzle.com>
2013-12-09 14:40:16 +0400

    version_watcher: code cleanup

--- lib/synapse/service_watcher/version.rb
+++ lib/synapse/service_watcher/version.rb
@@ -1,9 +1,9 @@
-require_relative "./base"
+require_relative "zookeeper"
 
 require 'zk'
 
 module Synapse
-  class VersionWatcher < BaseWatcher
+  class VersionWatcher < ZookeeperWatcher
     def start
       zk_hosts = @discovery['hosts'].shuffle.join(',')
 
@@ -13,12 +13,8 @@ module Synapse
       @zk = ZK.new(zk_hosts)
 
       # call the callback to bootstrap the process
+      watcher_callback.call
       version_watcher_callback.call
-      service_watcher_callback.call
-    end
-
-    def ping?
-      @zk.ping?
     end
 
     private
@@ -33,54 +29,7 @@ module Synapse
         unless @discovery['version_path']          
     end
 
-    # helper method that ensures that the discovery path exists
-    def create(path)
-      log.debug "synapse: creating ZK path: #{path}"
-      # recurse if the parent node does not exist
-      create File.dirname(path) unless @zk.exists? File.dirname(path)
-      @zk.create(path, ignore: :node_exists)
-    end
-
-    # find the current backends at the discovery path; sets @backends
-    def service_discover
-      log.info "synapse: discovering backends for service #{@name}"
-
-      new_backends = []
-      begin
-        @zk.children(@discovery['path'], :watch => true).map do |name|
-          node = @zk.get("#{@discovery['path']}/#{name}")
-
-          begin
-            host, port = deserialize_service_instance(node.first)
-          rescue
-            log.error "synapse: invalid data in ZK node #{name} at #{@discovery['path']}"
-          else
-            server_port = @server_port_override ? @server_port_override : port
-
-            log.debug "synapse: discovered backend #{name} at #{host}:#{server_port} for service #{@name}"
-            new_backends << { 'name' => name, 'host' => host, 'port' => server_port}
-          end
-        end
-      rescue ZK::Exceptions::NoNode
-        # the path must exist, otherwise watch callbacks will not work
-        create(@discovery['path'])
-        retry
-      end
-
-      if new_backends.empty?
-        if @default_servers.empty?
-          log.warn "synapse: no backends and no default servers for service #{@name}; using previous backends: #{@backends.inspect}"
-        else
-          log.warn "synapse: no backends for service #{@name}; using default servers: #{@default_servers.inspect}"
-          @backends = @default_servers
-        end
-      else
-        log.info "synapse: discovered #{new_backends.length} backends for service #{@name}"
-        @backends = new_backends
-      end
-    end
-
-    # find the current backends at the discovery path; sets @backends
+    # find the current version at the discovery path; update synapse config
     def version_discover
       log.info "synapse: discovering version for service #{@name}"
 
@@ -114,27 +63,9 @@ module Synapse
     end
     
     # sets up zookeeper callbacks if the data at the discovery path changes
-    def service_watch
-      @service_watcher.unsubscribe if defined? @service_watcher
-      @service_watcher = @zk.register(@discovery['path'], &service_watcher_callback)
-    end
-    
-    # sets up zookeeper callbacks if the data at the discovery path changes
     def version_watch
       @version_watcher.unsubscribe if defined? @version_watcher
       @version_watcher = @zk.register(@discovery['version_path'], &version_watcher_callback)
-    end
-
-    # handles the event that a watched path has changed in zookeeper
-    def service_watcher_callback
-      @service_callback ||= Proc.new do |event|
-        # Set new watcher
-        service_watch
-        # Rediscover
-        service_discover
-        # send a message to calling class to reconfigure
-        @synapse.reconfigure!
-      end
     end
     
     # handles the event that a watched path has changed in zookeeper
@@ -155,28 +86,5 @@ module Synapse
       end
     end
 
-    # tries to extract host/port from a json hash
-    def parse_json(data)
-      begin
-        json = JSON.parse data
-      rescue Object => o
-        return false
-      end
-      raise 'instance json data does not have host key' unless json.has_key?('host')
-      raise 'instance json data does not have port key' unless json.has_key?('port')
-      return json['host'], json['port']
-    end
-
-    # decode the data at a zookeeper endpoint
-    def deserialize_service_instance(data)
-      log.debug "synapse: deserializing process data"
-
-      # if that does not work, try json
-      host, port = parse_json(data)
-      return host, port if host
-
-      # if we got this far, then we have a problem
-      raise "could not decode this data:\n#{data}"
-    end
   end
 end
